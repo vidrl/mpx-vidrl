33e330840c4ce679dbe9ae084c3b6d9cf842746c
esteinig <eike.steinig@unimelb.edu.au>
2022-08-02 20:37:28 +1000

    debug completeness

--- mpx_assembly/report.py
+++ mpx_assembly/report.py
@@ -51,7 +51,7 @@ def get_consensus_assembly_data(file: Path) -> float:
     """
     Get consensus sequence and missing site proportion (N) - should only have a single sequence
     """
-    seq_data = [seq for seq in sequences.file_reader(file)]
+    seq_data = [seq for seq in sequences.file_reader(str(file))]
     seq = seq_data[0].seq
     return (1 - (seq.upper().count("N") / len(seq)))*100
 
