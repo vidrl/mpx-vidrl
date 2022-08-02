d0a6d9fd6f4b920fa0c1d0b14c815052d69ebbcf
esteinig <eike.steinig@unimelb.edu.au>
2022-08-02 20:20:25 +1000

    fix typer app

--- mpx_assembly/terminal.py
+++ mpx_assembly/terminal.py
@@ -2,16 +2,19 @@ import typer
 from pathlib import Path
 from .report import quality_control_consensus
 
-app = typer.Typer()
+app = typer.Typer(add_completion=False)
+
+report = typer.Typer()
+app.add_typer(report, name="report")
 
 
-@app.command()
-def report(
-    consensus_results: Path = typer.Argument(
+@report.command()
+def consensus(
+    results: Path = typer.Argument(
         ..., help="Output path of the Nextflow pipeline for consensus genome assembly"
     )
 ):
     """
     Main report interface to generate the report file from one or multiple samples
     """
-    quality_control_consensus(consensus_results=consensus_results)
+    quality_control_consensus(consensus_results=results)
