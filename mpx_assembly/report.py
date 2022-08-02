88efc5c4c2ec4a9e6d9381b537fdc5e74b7b7c28
esteinig <eike.steinig@unimelb.edu.au>
2022-08-03 08:24:58 +1000

    snp dist mat

--- mpx_assembly/report.py
+++ mpx_assembly/report.py
@@ -225,8 +225,6 @@ def snp_distance(dist: Path):
     for patient in patients_unique:
         # Within patient distances
         within_patient = dist_lower.loc[patient, patient]
-        print(within_patient)
-        print(type(within_patient))
         if isinstance(within_patient, numpy.float64) and numpy.isnan(within_patient):
             within_median = 0.
         else:
